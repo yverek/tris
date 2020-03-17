c176168aabc4d0e77f153a7011de7ffb142a1212
Mauro Dattilo <mau.dattilo@gmail.com>
2020-03-17 11:45:29 +0100

    Add constructor to Board component

--- src/index.js
+++ src/index.js
@@ -9,17 +9,24 @@ class Square extends React.Component {
             value: null
         }
     }
-    
+
     render() {
         return (
-            <button className="square" onClick={() => alert('click')}>
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+    
     renderSquare(i) {
         return <Square value={i} />;
     }
